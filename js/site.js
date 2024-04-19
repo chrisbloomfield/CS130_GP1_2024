e6371474aae2175b8facc3085b7d897b54d4be65
Sannteen <143964654+Sannteen@users.noreply.github.com>
2024-04-19 18:46:39 -0500

    modified

--- js/site.js
+++ js/site.js
@@ -5,3 +5,21 @@ function Clearform(){
     //document.getElementById('fullname').value = '';
     document.getElementById('contactform').reset();
 }
+
+/*const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))*/
+
+$(function(){
+    var pages=['index','about','travel','contact'];
+    var pathname = window.location.pathname;
+        
+    $('.nav-link').each(function(item){ 
+        if(pathname.includes(pages[item]))
+        {
+            $(this).addClass('active');
+            $(this).attr('aria-surrent', 'page');
+        }
+    } 
+    )
+}
+)
